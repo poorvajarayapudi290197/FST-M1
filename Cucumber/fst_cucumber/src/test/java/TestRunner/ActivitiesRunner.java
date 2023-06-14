58a8b10caa5f2b51ffb457ee9911c2ddf2aa363e
poorvajarayapudi290197 <132567577+poorvajarayapudi290197@users.noreply.github.com>
2023-06-14 20:08:39 +0530

    Add files via upload

---
+++ Cucumber/fst_cucumber/src/test/java/TestRunner/ActivitiesRunner.java
@@ -0,0 +1,16 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/Features",
+        glue = {"stepDefinitions"},
+        tags = "@activity1,@activity2,@activity3,@activity4,@activity5"
+)
+
+public class ActivitiesRunner {
+    //empty
+}
