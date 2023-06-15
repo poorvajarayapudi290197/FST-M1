6e526991f27b6d2e2d302ed272c3c79f30716ea3
poorvajarayapudi290197 <132567577+poorvajarayapudi290197@users.noreply.github.com>
2023-06-15 22:57:51 +0530

    Add files via upload

---
+++ Java/Activities/fst_module1/src/main/java/examples/Thirdclass.java
@@ -0,0 +1,7 @@
+package examples;
+
+public class Thirdclass {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ Java/Activities/fst_module1/src/main/java/examples/secondclass.java
@@ -0,0 +1,10 @@
+package examples;
+
+import java.util.Collections;
+
+public class secondclass {
+    public static void main(String args[]){
+        System.out.println("hello");
+        System.out.println();
+    }
+}
