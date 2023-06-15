d61eba2d9fa836b3035b63dba2c465499659bbbc
poorvajarayapudi290197 <132567577+poorvajarayapudi290197@users.noreply.github.com>
2023-06-15 23:20:45 +0530

    Add files via upload

---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,87 @@
+REM   Script: Activity 5
+REM   MY SCRIPTS
+
+create table firsttable( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+) 
+    Describe firsttable;
+
+Insert INTO firsttable 
+VALUES(5001, 'James Hoog', 'New York', 15);
+
+SELECT * FROM firsttable;
+
+create table firsttable( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+) 
+    Describe firsttable;
+
+INSERT INTO firsttable 
+VALUES(5001, 'James Hoog', 'New York', 15);
+
+SELECT * FROM firsttable;
+
+CREATE TABLE firsttable( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+) 
+    DESCRIBE firsttable;
+
+INSERT INTO firsttable 
+VALUES(5001, 'James Hoog', 'New York', 15);
+
+SELECT * FROM firsttable;
+
+CREATE TABLE firsttable( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+) 
+    DESCRIBE firsttable;
+
+INSERT INTO firsttable 
+VALUES(5001, 'James Hoog', 'New York', 15);
+
+SELECT * FROM firsttable;
+
+CREATE TABLE firsttable( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+    DESCRIBE firsttable
+
+
+INSERT INTO firsttable 
+VALUES(5001, 'James Hoog', 'New York', 15);
+
+SELECT * FROM firsttable;
+
+CREATE TABLE firsttable( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+    DESCRIBE firsttable
+
+
+INSERT INTO firsttable 
+VALUES(5001, 'James Hoog', 'New York', 15);
+
+SELECT * FROM firsttable;
+
+SELECT salesman_id, salesman_city FROM firsttable;
+
