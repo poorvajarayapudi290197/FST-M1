6b0d91e8a90fa762a09720425f735258cd2e83ab
poorvajarayapudi290197 <132567577+poorvajarayapudi290197@users.noreply.github.com>
2023-10-06 16:41:08 +0530

    Create Activity1.sql

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,16 @@
+REM   Script: Activity1
+REM   create table
+
+CREATE table customer2(CustomerId int PRIMARY KEY,CustomerName Varchar(52) NOT NULL,ContactName Varchar(32),Address Varchar(255),city Varchar(30),postalcode varchar(20),country varchar(20) 
+);
+
+DESCRIBE
+
+
+DESCRIBE customer2
+
+
+CREATE table salesman(salesman_id int PRIMARY KEY,salesman_name varchar2(20) NOT NULL,salesman_city varchar2(20),commission varchar2(20))  
+;
+
+DESCRIBE salesman
