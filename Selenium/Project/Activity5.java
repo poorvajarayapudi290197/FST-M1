89c9b0c7a9f4510e47f19b1d9097d53f1a95986d
poorvajarayapudi290197 <132567577+poorvajarayapudi290197@users.noreply.github.com>
2023-10-06 17:16:13 +0530

    Create Activity5.java

---
+++ Selenium/Project/Activity5.java
@@ -0,0 +1,31 @@
+package Activities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Activity5 {
+    public static void main(String args[]){
+        // Set up Firefox driver
+        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
+        WebDriverManager.firefoxdriver().setup();
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+
+        // Open the page
+        driver.get("https://alchemy.hguy.co/crm/");
+        //to login
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.id("user_name")).sendKeys("admin");
+        driver.findElement(By.id("username_password")).sendKeys("pa$$w0rd");
+        driver.findElement(By.id("bigbutton")).click();
+
+       //to get the color of the navigation menu
+        System.out.println(driver.findElement(By.id("toolbar")).getCssValue("color"));
+        driver.close();
+    }
+
+}
