e8adf6efbb5d050b78b65bdcf9910b21a43b97b9
poorvajarayapudi290197 <132567577+poorvajarayapudi290197@users.noreply.github.com>
2023-10-06 16:41:42 +0530

    Create Activity 2.sql

---
+++ SQL/Activities/Activity 2.sql
@@ -0,0 +1,35 @@
+REM   Script: session2
+REM   activity2
+
+CREATE table customer2(CustomerId int PRIMARY KEY,CustomerName Varchar(52) NOT NULL,ContactName Varchar(32),Address Varchar(255),city Varchar(30),postalcode varchar(20),country varchar(20) 
+);
+
+DESCRIBE customer2
+
+
+CREATE table salesman(salesman_id int PRIMARY KEY,salesman_name varchar2(20) NOT NULL,salesman_city varchar2(20),commission varchar2(20))  
+;
+
+DESCRIBE salesman
+
+
+INSERT ALL 
+    INTO salesman VALUES(5001, 'James Hoog', 'New York', 15) 
+    INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13) 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+select salesman_id,salesman_city from salesman;
+
+select * from salesman;
+
+select * from salesman where salesman_city='Paris';
+
+select * from salesman;
+
+select salesman_id,commission from salesman where salesman_name='Paul Adam';
